8b85bc483a3655bf090e6d69a4fcc7b4092e5d2f
silvia <frcuenca05@gmail.com>
2020-03-01 15:32:22 +0100

    Working code (not optimal)

--- src/models.py
+++ src/models.py
@@ -1,20 +1,60 @@
 class Book(object):
 
     def __init__(self, id: int, score: int):
-        self.id = id
-        self.score = score
+        self.id = int(id)
+        self.score = int(score)
+        self.scanned = False
+
+    def scan(self):
+        self.scanned = True
+
+    def can_scan(self):
+        return not self.scanned
 
     def __repr__(self):
-        return f'\n<Book {str(self.id)}: {str(self.score)}>'
+        # return f'\n(Book {str(self.id)}: {str(self.score)})'
+        return f'B{self.id}'
 
 
 class Library(object):
 
-    def __init__(self, id: int, signup_time: int, books_day: int, books: {Book}):
-        self.id = id
-        self.signup_time = signup_time
-        self.books_day = books_day
+    def __init__(self, id: int, signup_time: int, books_day: int, books: [Book]):
+        self.id = int(id)
+        self.signup_time = int(signup_time)
+        self.books_day = int(books_day)
         self.books = books
+        self.signed_up_start_day = -1
+
+    def start_sign_up(self, current_day):
+        self.signed_up_start_day = current_day
+
+    def is_not_signed_up(self):
+        return self.signed_up_start_day < 0
+
+    def is_signing_up(self, current_day):
+        return self.signed_up_start_day > -1 and current_day < self.signed_up_start_day + self.signup_time
+
+    def is_signed_up(self, current_day):
+        return self.signed_up_start_day > -1 and current_day >= self.signed_up_start_day + self.signup_time
+
+    def scan_books(self):
+        pending_books = [pending_book for pending_book in self.books if pending_book.can_scan()]
+        scanned_books = set()
+        books_to_scan = []
+        points = 0
+
+        if pending_books and len(pending_books) >= self.books_day:
+            books_to_scan = pending_books[:self.books_day]
+        elif pending_books:
+            books_to_scan = pending_books
+
+        for book in books_to_scan:
+            book.scan()
+            scanned_books.add(book)
+            points += book.score
+
+        return scanned_books, points
 
     def __repr__(self):
-        return f'\n[Library {str(self.id)}: Books: {self.books}]'
+        # return f'\n[Library {str(self.id)}: Books: {self.books}]'
+        return f'L{self.id}'
--- src/run.py
+++ src/run.py
@@ -1,8 +1,10 @@
+from operator import attrgetter
+
 from models import Book, Library
 
 filenames = [
     'examples/a_example.txt',
-    # 'examples/b_read_ontxt',
+    # 'examples/b_read_on.txt',
     # 'examples/c_incunabula.txt',
     # 'examples/d_tough_choices.txt',
     # 'examples/e_so_many_books.txt',
@@ -20,23 +22,25 @@ def build_books(scores_book: [int]):
 
 
 def search_books_by_id(books, library_books_ids):
-    library_books = set()
+    library_books = list()
 
     for book_idx in library_books_ids:
-        library_books.add(books[int(book_idx)])
+        library_books.append(books[int(book_idx)])
 
+    # sort books by book.score descendant
+    library_books.sort(key=lambda book: book.score, reverse=True)
     return library_books
 
 
 def read_input(lines: [str]):
     idx = 0
-    libraries = set()
+    libraries = list()
     while idx < len(lines):
         if idx == 0:
             elements = lines[idx].split()
             n_books = elements[0]
             n_libraries = elements[1]
-            days = elements[2]
+            days = int(elements[2])
 
             idx += 1
         elif idx == 1:
@@ -54,7 +58,7 @@ def read_input(lines: [str]):
             library_books = search_books_by_id(books, second_line_elements)
 
             library = Library(id=idx//2-1, signup_time=library_signup, books_day=library_books_day, books=library_books)
-            libraries.add(library)
+            libraries.append(library)
 
             if int(library_n_books) != len(library_books):
                 raise Exception('Input data is not consistent')
@@ -64,7 +68,66 @@ def read_input(lines: [str]):
     if int(n_books) != len(books) or int(n_libraries) != len(libraries):
         raise Exception('Input data is not consistent')
 
+    # sort books by book.score descendant
+    books.sort(key=lambda book: book.score, reverse=True)
+
+    # sort libraries by library.signup_time ascendant
+    libraries.sort(key=lambda library: library.signup_time, reverse=False)
+
     return books, libraries, days
+
+
+def check_is_current_open_process(libraries, current_day):
+    for library in libraries:
+        if library.is_signing_up(current_day):
+            return True
+
+    return False
+
+
+def start_sign_up_process(libraries, current_day):
+    not_signed_up_libraries = [library for library in libraries if library.is_not_signed_up()]
+
+    if not_signed_up_libraries:
+        library_to_start_sign_up = not_signed_up_libraries[0]
+        library_to_start_sign_up.start_sign_up(current_day)
+        return library_to_start_sign_up
+
+    return None
+
+
+def get_daily_work(current_day, libraries, books, all_daily_work):
+    """
+    Calculate the process per day per library.
+    Return a list of tuple and the total points from this day.
+    The list contains a tuple with each library that works this day (signing up or scanning)
+    and the set of books scanned this day. If the scanned book list is empty, this day is the
+    signing up process.
+
+    :param current_day:
+    :param libraries:
+    :param books:
+    :param all_daily_work:
+    :return: [Tuple(Library, {Books})], int
+    """
+    signing_up_process = check_is_current_open_process(libraries=libraries, current_day=current_day)
+    daily_points = 0
+
+    daily = list()
+    if not signing_up_process:
+        library = start_sign_up_process(libraries=libraries, current_day=current_day)
+        if library:
+            daily.append((library, None))
+
+    libraries_ready_to_scan = {library_ready for library_ready in libraries if library_ready.is_signed_up(current_day)}
+    for library_ready in libraries_ready_to_scan:
+        new_scanned_books, points = library_ready.scan_books()
+
+        if new_scanned_books:
+            daily.append((library_ready, new_scanned_books))
+            daily_points += points
+
+    return daily, daily_points
 
 
 if __name__ == '__main__':
@@ -76,6 +139,19 @@ if __name__ == '__main__':
 
         books, libraries, days = read_input(lines)
 
-        # total += points
+        current_day = 0
+        points = 0
+        all_daily_work = list()
+        while current_day < days:
+
+            daily_work, daily_points = get_daily_work(current_day, libraries, books, all_daily_work)
+
+            all_daily_work.append(daily_work)
+            points += daily_points
+            current_day += 1
+
+        total += points
+
+        print(all_daily_work)
 
     print('Total: ' + str(total))
